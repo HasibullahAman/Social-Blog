bb64c144763d54b9a054c6e421c3eceb64aa9e62
Hasibullah Aman <hasib.aman7@gmail.com>
2024-05-25 15:06:10 +0430

    render a page

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse("<h1 style = background-color:red;>Blog About</h1>")
+    return render(request, 'blog/about.html')
+
+# def home(request):
+#     return HttpResponse("<h1>Blog Home</h1>")
+
+
+# def about(request):
+#     return HttpResponse("<h1 style = background-color:red;>Blog About</h1>")
 
 
