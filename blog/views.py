be5179f2979c5c41b5b71cdcc8feff558f70d690
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-04 18:07:03 +0430

    start to create post

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,25 @@ from django.http import HttpResponse
 
 
 
+posts = [
+    {
+        'author': 'Hasibullah Aman',
+        'title': 'Blog post 1',
+        'content': 'first post content',
+        'date_posted': 'August 27, 2024'
+        },
+    {
+        'author': 'Nawidullah Aman',
+        'title': 'Blog post 2',
+        'content': 'second post content',
+        'date_posted': 'August 30, 2024'
+    }
+]
+
 def home(request):
+    context = {
+        'post': posts 
+    }
     return render(request, 'blog/home.html')
 
 
